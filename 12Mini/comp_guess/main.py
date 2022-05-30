13b7a86bb946ec461af8c8cc58e6d6db505f3e74
Kamallakannan Sekar <kamallakannansekar@Kamallakannans-MacBook-Pro.local>
2022-05-30 15:00:43 +0530

    3rd Program2.change

--- 12Mini/comp_guess/main.py
+++ 12Mini/comp_guess/main.py
@@ -10,13 +10,10 @@ def comp_guess(x):
         feedback = input(f"Is {guess} is low/high/correct  :")
         if feedback == 'l':
             print(f"Too Low, Guess Again")
-
             min = guess + 1
         elif feedback == 'h':
             print("Too High, Guess Again")
-
             max = guess - 1
         else:
             print(f"You guessed it correctly {guess}")
-
 comp_guess(100)
